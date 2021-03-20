0ff8bc0822b983254a914f2c98a194b8742eb5ed
ubedp <ubedpatait414@gmail.com>
2021-03-20 20:29:52 +0530

    java script programs

---
+++ tec.js
@@ -0,0 +1,74 @@
+// let year;
+// year=366;
+
+// if(year%4==1)
+// {let
+//     console.log("normal year");
+// }
+// else{
+//     console.log("leap year");
+// }
+
+
+//
+let sub1,sub2,sub3,sub4;
+sub1 = 50;
+sub2 = 50;
+sub3 = 30;
+sub4 = 55;
+
+if(sub1 <35 || sub2 <35 || sub3 <35 || sub4 <35){
+    console.log("fail");
+} else {
+    total = sub1+sub2+sub3+sub4;
+    percent = total/4*100;
+    if(percent >=75){
+        console.log("Grade A");
+    }else if(percent >=60){
+        console.log("Grade B");
+     } else if (percent < 60){
+         console.log("c");
+     }
+}
+
+
+
+//valentine day
+
+let valentineday='7 feb'
+console.log("rose day");
+
+switch(valentineday)
+{
+case "7 feb":
+    console.log("rodeday");
+
+    case "8 feb":
+    console.log("rodeday");
+
+    case "9 feb":
+    console.log("rodeday");
+
+    case "10 feb":
+    console.log("rodeday");
+
+    case "11 feb":
+    console.log("rodeday");
+
+    case "12 feb":
+    console.log("rodeday");
+
+    case "13 feb":
+    console.log("rodeday");
+    break;
+
+    case "14 feb":
+    console.log("rodeday");
+    break;
+
+    default:
+        console.log("plaese");
+
+   
+}
+
